ea6837c0eda887c3f13fc7d98602bc4cfee2451b
air <kobychev00@bk.ru>
2023-03-09 12:20:43 +0300

    акшые сщььше 09.03.22

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Tutor karpov = new Tutor(
+                "Карпов Денис",
+                "Языковой центр",
+                "Руководитель кружка",
+                "Английский язык",
+                1);
+        Tutor kerimova = new Tutor(
+                "Керимова Наталья",
+                "Детский сад",
+                "Шеф-повар",
+                null,
+                2);
+        Tutor kugut = new Tutor(
+                "Кугут Виктория",
+                "Детский сад",
+                "Воспитатель",
+                null,
+                3);
+        Tutor mikhailova = new Tutor(
+                "Михайлова Екатерина",
+                "Детский сад",
+                "Методист",
+                "Сказочный Петербург",
+                1);
+    }
+}
---
+++ src/Tutor.java
@@ -0,0 +1,15 @@
+public class Tutor {
+    private String fullName;
+    private String departament;
+    private String position;
+    private String subject;
+    private int id;
+
+    public Tutor(String fullName, String departament, String position, String subject, int id) {
+        this.fullName = fullName;
+        this.departament = departament;
+        this.position = position;
+        this.subject = subject;
+        this.id = id;
+    }
+}
